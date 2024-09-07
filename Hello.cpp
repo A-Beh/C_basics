e143ea7cd4b8ca67260fc48bb2114f74fabffbcb
A-Beh <a.behfarnia@hotmail.com>
2024-09-07 13:53:25 -0500

    start print created

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int file_size = 0, cnt = 101;
+    double sales = 9.99;
+    cout << "Hello World \n"; // cout stands for character out
+    cout << "file_size = " <<file_size<< "\n";
+    cout << "Count = " << cnt << "\n";
+    cout << "Sales = " << sales << "\n";
+    return 0; // 0 tells the operating system that our program terminates correctly
+}
